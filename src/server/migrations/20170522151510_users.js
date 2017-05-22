1b28c4feca9f5eb797b2d52b2617b5e74d4287a3
Evan Busse <evanbusse@gmail.com>
2017-05-22 15:30:00 -0600

    migrations set up and working

--- src/server/migrations/20170522151510_users.js
+++ src/server/migrations/20170522151510_users.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  knex.schema.createTable('users', (table) => {
+    table.increments('id');
+    table.string('email').notNullable().defaultsTo('');
+    table.string('password').notNullable().defaultsTo('');
+    table.string('password').notNullable().defaultsTo('');
+    table.text('profileUrl').notNullable().defaultsTo('http://www.zwani.com/graphics/hello_funny/images/56467.jpg');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  knex.schema.dropTable('users');
 };
