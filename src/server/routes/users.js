ad2373e9cfd4baad24646844cd41390e4d25574e
Evan Busse <evanbusse@gmail.com>
2017-05-29 20:00:14 -0600

    bcrypt checking passwords succesfully

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -12,6 +12,7 @@ const bcrypt = require('bcrypt');
 
 //User Routes
 
+//Register a new user
 router.post('/users/new', (req, res) => {
   var {name, email, password, profileUrl} = req.body;
 
@@ -29,10 +30,12 @@ router.post('/users/new', (req, res) => {
   .then((dataCheck) => {
     //Check to see if email already exists
     if(dataCheck.length > 0){
+      console.log('user already found');
       res.send('An account using that email already exists')
     }else{
       console.log('Creating new account');
-
+      //Insert new user into the database
+      //!Further checking needed for production build!
       knex('users')
       .insert(newUser, "*")
       .then((userData) => {
@@ -40,7 +43,31 @@ router.post('/users/new', (req, res) => {
       })
     }
   })
+})
 
+router.get('/users/login', (req, res) => {
+  var {email, password} = req.body;
+
+  if(email && password) {
+    //Get users hashed password
+    knex('users')
+    .where('email', email)
+    .then((userData) => {
+      if(userData.length > 0) {
+        userData = userData[0];
+
+        //compare passwords
+        if(bcrypt.compareSync(password, userData.password)){
+          res.send(userData)
+        }else{
+          res.send('incorrect password')
+        }
+      }else{
+        //Response if no user
+        res.send('No user with that email found')
+      }
+    })
+  }
 })
 
 module.exports = router;
